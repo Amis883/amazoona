983396732fa28772ed7754c0004dad0e01c09d34
farazmehraien95 <farazmehraien95@gmail.com>
2022-03-23 23:24:29 +0430

    fix thunk

--- amazon/src/actions/productActions.js
+++ amazon/src/actions/productActions.js
@@ -5,13 +5,16 @@ import {
 } from "../constants/productConstants";
 import axios from "axios";
 
-export const listProducts = async (dispatch) => {
-  dispatch({ type: PRODUCT_LIST_REQUEST });
-  try {
-    const { data } = await axios.get("api/products/");
-    console.log(data.products);
-    dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
-  } catch (error) {
-    dispatch({ type: PRODUCT_LIST_FAIL, payload: error.message });
+export const listProducts =  () => {
+  return async (dispatch)=>{
+    dispatch({ type: PRODUCT_LIST_REQUEST });
+    try {
+    const response  = await axios.get("api/products/");
+    console.log('axios response',response);
+      return dispatch({ type: PRODUCT_LIST_SUCCESS, payload: response })
+    } catch (error) {
+      console.log('axios error',error);
+      return dispatch({ type: PRODUCT_LIST_FAIL, payload: error.message })
+    }
   }
 };

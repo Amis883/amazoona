e969b47d075ecda8801b41da139360e545b67527
amis883 <hodakakhki@Sinansoft.com>
2022-03-25 14:42:20 +0430

    Implement Add To Cart

--- amazon/src/App.js
+++ amazon/src/App.js
@@ -1,22 +1,30 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import CartScreen from "./screen/CartScreen";
 import HomeScreen from "./screen/HomeScreen";
 import ProductScreen from "./screen/ProductScreen";
+import { useSelector } from "react-redux";
 
 function App() {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
   return (
     <BrowserRouter>
       <div className="grid-container">
         <header className="row">
           <div>
-            <a href="/" className="brand">
+            <Link to="/" className="brand">
               amazona
-            </a>
+            </Link>
           </div>
           <div>
-            <a href="/Cart">Cart</a>
-            <a href="/signin">Sign In</a>
+            <Link to="/Cart">
+              Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            <Link to="/signin">Sign In</Link>
           </div>
         </header>
         <main>
--- amazon/src/screen/CartScreen.js
+++ amazon/src/screen/CartScreen.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { useSearchParams } from "react-router-dom";
 import { useLocation, useParams } from "react-router-dom";
+import { addToCart } from "../actions/cartActions";
 
 export default function CartScreen() {
   const { id } = useParams();
   const { search } = useLocation();
   const [searchParams] = useSearchParams();
-
   const productId = id;
   const qty = search ? Number(search.split("=")[1]) : 1;
 
   console.log({ productId, qty, qtyParam: Number(searchParams.get("qty")) });
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+
   return (
     <div>
       <h1>CartScreen</h1>

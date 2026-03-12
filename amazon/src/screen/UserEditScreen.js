8b7a0140a4a5b6d3a0fd9ca85f24aacbe8f672ae
Hoda ka <hodakakhki@Sinansoft.com>
2026-03-12 01:04:44 +0100

    fix router import

--- amazon/src/screen/UserEditScreen.js
+++ amazon/src/screen/UserEditScreen.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
-import { useNavigate } from "../../node_modules/react-router-dom/index";
+import { useNavigate, useParams } from "react-router-dom";
 import { detailsUser, updateUser } from "../actions/userActions";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
